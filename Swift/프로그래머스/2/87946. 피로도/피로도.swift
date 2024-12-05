61ed2c29db5026b6ce1841c306470b1d4194e69b
Crois0509 <crois0509@naver.com>
2024-12-05 09:13:58 +0900

    [level 2] Title: 피로도, Time: 32.93 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ Swift/프로그래머스/2/87946. 피로도/피로도.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
+    var answer = 0
+    
+    var visited = Array(repeating: false, count: dungeons.count)
+    
+    func dfs(_ currentFatigue: Int, _ count: Int) {
+        answer = max(answer, count)
+        
+        for i in dungeons.indices {
+            let minRequired = dungeons[i][0]
+            let fatigueCost = dungeons[i][1]
+            
+            if !visited[i] && currentFatigue >= minRequired {
+                visited[i] = true
+                
+                dfs(currentFatigue - fatigueCost, count + 1)
+                visited[i] = false
+            } else {
+                continue
+            }
+        }
+    }
+    
+    dfs(k, 0)
+    
+    return answer
+}
