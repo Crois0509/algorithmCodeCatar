74551ee3bc1ae8974fdbb02e8cf4c54d874b09cb
Crois <crois0509@naver.com>
2024-12-19 09:18:57 +0900

    [level 3] Title: 단어 변환, Time: 0.25 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ Swift/프로그래머스/3/43163. 단어 변환/단어 변환.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
+    guard words.contains(target) else { return 0 } // target이 없을 경우 0 반환 후 종료
+    guard begin != target else { return 0 }
+    
+    var answer: Int = 0
+    var chars: Set<String> = []
+    
+    var result: String = begin
+    
+    var index: Int = 0
+    
+    while result != target {
+        for word in words {
+            let currentWord = zip(result, target).filter { $0 != $1 }.count == 1
+            guard !currentWord else { return index + 1 }
+            
+            let check = zip(result, word).filter { $0 != $1 }.count == 1
+            
+            if check && !chars.contains(word) {
+                chars.insert(word)
+                result = word
+                index += 1
+            } else {
+                continue
+            }
+        }
+    }
+    
+    answer = index
+    
+    return answer
+}
