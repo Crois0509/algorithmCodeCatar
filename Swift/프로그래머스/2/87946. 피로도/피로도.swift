9e9d04c9bb9e28f66be8e494a66bc67982b38ee0
Crois <crois0509@naver.com>
2025-07-16 14:52:40 +0900

    [level 2] Title: 피로도, Time: 36.21 ms, Memory: 16.5 MB -BaekjoonHub

--- Swift/프로그래머스/2/87946. 피로도/피로도.swift
+++ Swift/프로그래머스/2/87946. 피로도/피로도.swift
@@ -1,31 +1,28 @@
 import Foundation
 
-func solution(_ k: Int, _ dungeons: [[Int]]) -> Int {
-    var answer: Int = 0
-    var figure = k
-    var startCount = 0
-    var dungenonsCheck: [Bool] = Array(repeating: false, count: dungeons.count)
+func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
+    var answer = 0
+    var visited = Array(repeating: false, count: dungeons.count)
     
-    func dfs(_ count: inout Int, _ currentFigure: inout Int) {
+    func dfs(_ currentFatigue: Int, _ count: Int) {
         answer = max(answer, count)
         
-        for (index, dungeon) in dungeons.enumerated() {
-            guard dungenonsCheck[index] == false else { continue }
-            guard currentFigure >= dungeon[0] else { continue }
+        for i in dungeons.indices {
+            let minRequired = dungeons[i][0]
+            let fatigueCost = dungeons[i][1]
             
-            count += 1
-            currentFigure -= dungeon[1]
-            dungenonsCheck[index] = true
-            
-            dfs(&count, &currentFigure)
-            
-            dungenonsCheck[index] = false
-            count -= 1
-            currentFigure += dungeon[1]
+            if !visited[i] && currentFatigue >= minRequired {
+                visited[i] = true
+                
+                dfs(currentFatigue - fatigueCost, count + 1)
+                visited[i] = false
+            } else {
+                continue
+            }
         }
     }
     
-    dfs(&startCount, &figure)
+    dfs(k, 0)
     
     return answer
 }
