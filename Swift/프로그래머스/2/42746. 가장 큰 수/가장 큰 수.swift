4d212eca44636285b4af0afc5d19bfc45028b004
Crois <crois0509@naver.com>
2025-08-06 15:24:53 +0900

    [level 2] Title: 가장 큰 수, Time: 467.63 ms, Memory: 23 MB -BaekjoonHub

---
+++ Swift/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> String {
+    let sorted = numbers.map { String($0) }.sorted {
+        $0 + $1 > $1 + $0
+    }
+    
+    let result = sorted.joined()
+    
+    return result.first == "0" ? "0" : result
+}
