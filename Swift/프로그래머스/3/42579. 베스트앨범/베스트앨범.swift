c09acdb95b5fc5a2d7f9cd67f25ed9afabfa110c
Crois <crois0509@naver.com>
2025-10-14 15:12:09 +0900

    [level 3] Title: 베스트앨범, Time: 0.55 ms, Memory: 16.3 MB -BaekjoonHub

--- Swift/프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
+++ Swift/프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -5,16 +5,16 @@ func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
     var music = [String: [(n: Int, play: Int)]]()
     var answer = [Int]()
     
-    for n in 0..<genres.count {
-        let genre = genres[n]
-        let play = plays[n]
+    for i in genres.indices {
+        let genre = genres[i]
+        let play = plays[i]
         
         played[genre, default: 0] += play
-        music[genre, default: []].append((n, play))
+        music[genre, default: []].append((i, play))
     }
     
     for (genre, _) in played.sorted(by: { $0.value > $1.value }) {
-        let albums = music[genre]!.sorted{
+        let albums = music[genre]!.sorted {
             if $0.play == $1.play {
                 return $0.n < $1.n
             }
