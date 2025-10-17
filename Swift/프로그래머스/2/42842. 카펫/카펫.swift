13953ee7ee225e23426e5ca97833211f57def154
Crois <crois0509@naver.com>
2025-10-17 15:28:11 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

--- Swift/프로그래머스/2/42842. 카펫/카펫.swift
+++ Swift/프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,17 +1,34 @@
 import Foundation
 
-func solution(_ brown: Int, _ yellow: Int) -> [Int] {
-    let total = brown + yellow
-    
-    for height in 3...total {
-        if total % height != 0 { continue }
-        
-        let width = total / height
-        
-        if (width - 2) * (height - 2) == yellow {
-            return [width, height]
+func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+    var width: Int = 0
+    var height: Int = 0
+    var index: Int = 1
+    var current: Int {
+        get {
+            return (width * 2) + (height * 2) - 4
         }
     }
     
-    return []
+    while current != brown {
+        guard yellow % index == 0,
+              yellow % (yellow / index) == 0
+        else {
+            index += 1
+            continue
+        }
+        
+        let yellowTile = yellow / index
+        
+        width = yellowTile + 2
+        height = (yellow / yellowTile) + 2
+        
+        index += 1
+    }
+    
+    if width >= height {
+        return [width, height]
+    } else {
+        return [height, width]
+    }
 }
