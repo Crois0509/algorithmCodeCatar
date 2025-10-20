4e9c61e0e5ae54fb3593159ff34dd3b06a56f802
Crois <crois0509@naver.com>
2025-10-20 10:51:42 +0900

    [level 2] Title: 올바른 괄호, Time: 10.98 ms, Memory: 16.6 MB -BaekjoonHub

--- Swift/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
+++ Swift/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 func solution(_ s:String) -> Bool {
-    var answer :Bool = false
-    var count: Int = 0
+    var stack: [Character] = []
     
     for char in s {
         if char == "(" {
-            count += 1
+            stack.append(char)
         } else {
-            count -= 1
+            if stack.isEmpty {
+                return false
+            } else {
+                stack.removeLast()
+            }
         }
-        guard count >= 0 else { return false }
     }
-
-    if count == 0 { answer = true }
     
-    return answer
+    return stack.isEmpty
 }
