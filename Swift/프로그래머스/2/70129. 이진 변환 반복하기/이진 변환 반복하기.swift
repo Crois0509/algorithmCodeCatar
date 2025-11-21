1e51969aca2a8a474426e6569d8b0047c81f6f3c
Crois <crois0509@naver.com>
2025-11-21 09:56:20 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 65.11 ms, Memory: 20.7 MB -BaekjoonHub

--- Swift/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.swift
+++ Swift/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.swift
@@ -1,16 +1,25 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
+    var result = s
     var count: Int = 0
-    var result: String = s
-    var zeroCount: Int = 0
+    var zeroCount: [String] = []
     
     while result != "1" {
+        if result.contains("0") {
+            let one = result.filter { $0 == "1" }
+            let zero = result.filter { $0 == "0" }.map { String($0) }
+            
+            zeroCount.append(contentsOf: zero)
+            result = one
+            
+        }
+        
+        let num = result.count
+        let newNum = String(num, radix: 2)
+        result = newNum
         count += 1
-        zeroCount += result.filter { $0 == "0" }.count
-        let decimalNumber = result.filter { $0 == "1" }.count
-        result = String(decimalNumber, radix: 2)
     }
     
-    return [count, zeroCount]
+    return [count, zeroCount.count]
 }
