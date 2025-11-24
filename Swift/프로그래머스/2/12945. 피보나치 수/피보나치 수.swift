9f1f5d952e7c65ddb39ab3903bb4e27e8070cc49
Crois <crois0509@naver.com>
2025-11-24 11:00:48 +0900

    [level 2] Title: 피보나치 수, Time: 3.03 ms, Memory: 16.7 MB -BaekjoonHub

--- Swift/프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
+++ Swift/프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
@@ -1,9 +1,14 @@
 func solution(_ n:Int) -> Int {
-    var nums = [0, 1]
+    var nums: [Int] = [0, 1]
     
-    for i in 1..<n {
-        nums.append((nums[i] + nums[i-1]) % 1234567)
+    for i in 2...n {
+        let newNum = (nums[i - 1] + nums [i - 2]) % 1234567
+        nums.append(newNum)
     }
     
-    return nums[n]
+    if let last = nums.last {
+        return last
+    } else {
+        return 0
+    }
 }
