adcaa6f635e072394410c8d90de9d0ea2ac50a1a
Crois <crois0509@naver.com>
2025-11-27 09:38:18 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- Swift/프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
+++ Swift/프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 func solution(_ n:Int, _ a:Int, _ b:Int) -> Int {
-    var playerA = a
-    var playerB = b
+    var a = a
+    var b = b
     var round = 0
     
-    while playerA != playerB {
-        playerA = (playerA + 1) / 2
-        playerB = (playerB + 1) / 2
+    while a != b {
+        a = (a + 1) / 2
+        b = (b + 1) / 2
         round += 1
     }
     
