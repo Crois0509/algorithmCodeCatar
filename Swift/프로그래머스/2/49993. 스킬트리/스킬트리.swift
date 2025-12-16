80cd52048de149327dda8fdbbd8d2e87d35a122d
Crois <crois0509@naver.com>
2025-12-16 11:29:56 +0900

    [level 2] Title: 스킬트리, Time: 0.07 ms, Memory: 16.2 MB -BaekjoonHub

--- Swift/프로그래머스/2/49993. 스킬트리/스킬트리.swift
+++ Swift/프로그래머스/2/49993. 스킬트리/스킬트리.swift
@@ -1,41 +1,27 @@
 import Foundation
 
-func solution(_ skill:String, _ skill_trees:[String]) -> Int {
-    var index: Int = 0
-    var answer: Int = 0
-    var dic: [Character: Int] = [:]
-    
-    for (i, element) in skill.enumerated() {
-        let beforIndex = i - 1
-        dic[element] = beforIndex
-    }
-    
-    while index < skill_trees.count {
-        
-        let skillTree = skill_trees[index]
-        var skills = Array(repeating: false, count: skill.count)
-        
-        for (i, char) in skillTree.enumerated() {
-            
-            if let beforIndex = dic[char] {
-                if beforIndex < 0 {
-                    skills[0] = true
-                    
-                } else if skills[beforIndex] == true {
-                    skills[beforIndex + 1] = true
-                    
-                } else {
+func solution(_ skill: String, _ skill_trees: [String]) -> Int {
+    let skillArray = Array(skill)
+    var answer = 0
+
+    for tree in skill_trees {
+        var index = 0
+        var isValid = true
+
+        for ch in tree {
+            if let pos = skillArray.firstIndex(of: ch) {
+                if pos != index {
+                    isValid = false
                     break
                 }
-            }
-            
-            if i == (skillTree.count - 1) {
-                answer += 1
+                index += 1
             }
         }
-        
-        index += 1
+
+        if isValid {
+            answer += 1
+        }
     }
-    
+
     return answer
 }
