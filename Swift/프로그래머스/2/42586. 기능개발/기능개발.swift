aed259619ce2ae84c31f898f9b97bab34ab69094
Crois <crois0509@naver.com>
2026-02-23 12:41:33 +0900

    [level 2] Title: 기능개발, Time: 0.20 ms, Memory: 16.7 MB -BaekjoonHub

--- Swift/프로그래머스/2/42586. 기능개발/기능개발.swift
+++ Swift/프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -1,24 +1,26 @@
 import Foundation
 
-func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
-    let daysNeeded = zip(progresses, speeds).map { (progress, speed) in
-        return (100 - progress + speed - 1) / speed
+func solution(_ progresses: [Int], _ speeds: [Int]) -> [Int] {
+    // 1. 각 작업의 배포 가능일까지 걸리는 시간을 미리 계산 (정수 연산 올림 팁)
+    let days = zip(progresses, speeds).map { (p, s) in
+        (100 - p + s - 1) / s
     }
     
-    var result: [Int] = []
-    var deployDay = daysNeeded[0]
+    var answer = [Int]()
+    var maxDay = days[0]
     var count = 0
     
-    for day in daysNeeded {
-        if day <= deployDay {
+    // 2. 순회하며 그룹화
+    for day in days {
+        if day <= maxDay {
             count += 1
         } else {
-            result.append(count)
-            deployDay = day
+            answer.append(count)
             count = 1
+            maxDay = day
         }
     }
+    answer.append(count)
     
-    result.append(count)
-    return result
+    return answer
 }
