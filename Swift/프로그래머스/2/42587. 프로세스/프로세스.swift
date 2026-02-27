11d0c34f8c914b331a7371fe69cf00a0456f91ee
Crois <crois0509@naver.com>
2026-02-27 11:33:52 +0900

    [level 2] Title: 프로세스, Time: 0.61 ms, Memory: 16.7 MB -BaekjoonHub

--- Swift/프로그래머스/2/42587. 프로세스/프로세스.swift
+++ Swift/프로그래머스/2/42587. 프로세스/프로세스.swift
@@ -1,25 +1,30 @@
 import Foundation
 
-func solution(_ priorities:[Int], _ location:Int) -> Int {
-    let target = (priorities[location], location)
-    var queue: [(num: Int, location: Int)] = priorities.enumerated().map { ($1, $0) }
-    var visit = [Bool](repeating: false, count: queue.count)
+func solution(_ priorities: [Int], _ location: Int) -> Int {
+    var values = priorities.enumerated().map { (num: $1, location: $0) }
+    var sortedPriorities = priorities.sorted(by: >) // 우선순위만 정렬해둡니다 😈✨
+    var index = 0
+    var inputCount = 0
     
-    while !queue.isEmpty {
-        let current = queue.removeFirst()
+    while !values.isEmpty {
+        let current = values[index]
         
-        if queue.contains(where: { $0.num > current.num }) {
-            queue.append(current)
-            continue
+        // 현재 위치의 중요도가 가장 높은 순서와 일치하는지 확인
+        if current.num == sortedPriorities.first! {
+            inputCount += 1
+            sortedPriorities.removeFirst() // 실행했으므로 목록에서 제거            
+            values.remove(at: index) // 실행된 프로세스는 제거 (혹은 visit 처리)
             
-        } else {
-            visit[current.location] = true
-            
-            if current == target {
-                return visit.filter { $0 }.count
+            if current.location == location {
+                return inputCount
             }
+            
+            // 값을 제거했다면 index는 그대로 유지 (자동으로 다음 원소를 가리킴)
+            if index >= values.count { index = 0 }
+        } else {
+            // 더 높은 순위가 있다면 다음으로 이동
+            index = (index + 1) % values.count
         }
     }
-    
     return 0
 }
