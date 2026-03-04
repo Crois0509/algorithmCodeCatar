ec2dd6618a820b00650b89523097a121e2f822af
Crois <crois0509@naver.com>
2026-03-04 11:03:57 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 16.3 MB -BaekjoonHub

--- Swift/프로그래머스/2/42578. 의상/의상.swift
+++ Swift/프로그래머스/2/42578. 의상/의상.swift
@@ -1,16 +1,9 @@
 import Foundation
 
-func solution(_ clothes:[[String]]) -> Int {
-    var dic = [String:Int]()
-    var answer = 0
-    
-    for cloth in clothes {
-        let category = cloth[1]
-        dic[category, default: 0] += 1
-    }
-    
-    let total = dic.values.reduce(1) { $0 * ($1 + 1) }
-    answer = total - 1
-    
-    return answer
+func solution(_ clothes: [[String]]) -> Int {
+    // 1. 종류별로 묶어서 개수만 파악합니다.
+    let grouped = Dictionary(grouping: clothes) { $0[1] }
+
+    // 2. 각 종류별 (개수 + 1)을 모두 곱한 뒤 1을 뺍니다.
+    return grouped.values.reduce(1) { $0 * ($1.count + 1) } - 1
 }
