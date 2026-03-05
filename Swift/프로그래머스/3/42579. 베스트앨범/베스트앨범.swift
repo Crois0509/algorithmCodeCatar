9f0bdd6431d2ed7cfd51a89111e162cc84aee8c1
Crois <crois0509@naver.com>
2026-03-05 11:36:04 +0900

    [level 3] Title: 베스트앨범, Time: 0.36 ms, Memory: 16.7 MB -BaekjoonHub

--- Swift/프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
+++ Swift/프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -1,28 +1,28 @@
 import Foundation
 
+struct Album {
+    let index: Int
+    let play: Int
+}
+
 func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
-    var playList: [String: [(i: Int, play: Int)]] = [:]
-    var genreList: [String:Int] = [:]
+    
+    var dic = [String:[Album]]()
     var answer: [Int] = []
     
-    for i in genres.indices {
-        let genre = genres[i]
-        let play = plays[i]
-        
-        playList[genre, default: []].append((i, play))
-        genreList[genre, default: 0] += play
-        
-        playList[genre]?.sort(by: { $0.play > $1.play })
+    for i in 0..<genres.count {
+        let album = Album(index: i, play: plays[i])
+        dic[genres[i], default: []].append(album)
     }
     
-    for album in genreList.sorted(by: { $0.value > $1.value }) {
-        let list = playList[album.key]!
-        
-        if list.count < 2 {
-            answer.append(list.first!.i)
-        } else {
-            answer.append(contentsOf: [list[0].i, list[1].i])
-        }
+    let sorted = dic.sorted(by: {
+        $0.value.reduce(0) { $0 + $1.play } > $1.value.reduce(0) { $0 + $1.play }
+    })
+    
+    for album in sorted {
+        let top = Array(album.value.sorted(by: { $0.play > $1.play }).prefix(2))
+        let result = top.map { $0.index }
+        answer.append(contentsOf: result)
     }
     
     return answer
